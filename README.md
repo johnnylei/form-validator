f02ae314c9e1c391a89f78c3867a7f1e09ee8834
johnnye <johnny@johnny.com>
2018-02-04 00:17:44 +0800

    update

--- README.md
+++ README.md
@@ -1,2 +1,110 @@
 # form-validator
 这是一个form表单验证器，基于jquery
+
+# usage
+```
+$('#some-form').formValidator();
+```
+
+#基本使用说明
+## 最基本的demo
+```
+<form>
+<div class='field-item'><input class='need-validate'><div class='error'></div></div>
+<button type='submit'>
+</form>
+
+- 你所有的需要验证的字段必须要加上class='need-validate'
+- 所有字段必要要一个包裹在class='field-item'的区域里面
+- 所有字段都需要一个错误显示的区域, 且class为error,展示error的信息
+```
+## 必填demo
+```
+<form>
+<div class='field-item'><input class='need-validate required'><div class='error'></div></div>
+<button type='submit'>
+</form>
+
+- class required
+```
+## 数字
+```
+<form>
+<div class='field-item'><input class='need-validate number'><div class='error'></div></div>
+<button type='submit'>
+</form>
+
+- class number
+```
+## email
+```
+<form>
+<div class='field-item'><input class='need-validate email'><div class='error'></div></div>
+<button type='submit'>
+</form>
+
+- class number
+```
+## 正则
+
+```
+<form>
+<div class='field-item'><input class='need-validate match' data-pattern='\d+'><div class='error'></div></div>
+<button type='submit'>
+</form>
+
+- class number
+- data-pattern里面填写正则表达式
+```
+#事件
+```
+var events = {
+    // 在init之前触发，由form元素触发
+    beforeInit : 'beforeInit',
+    // 在init之后触发，由form元素触发
+    afterInit : 'afterInit',
+    // 验证的时候触发，由form触发
+    beforeValidate : 'beforeValidate',
+    afterValidate : 'afterValidate',
+    // 系统使用
+    validateField : 'validate.field',
+    // 单个字段验证的时候触发，由具体的字段触发
+    beforeValidateField : 'beforeValidateField',
+    afterValidateField : 'afterValidateField'
+};
+```
+# 高级使用
+```
+var params = {
+    form_options : {
+        // 配置需要验证的字段的选择器,默认class='need-validate'
+        need_validate_selector : '.need-validate',
+        // 配置每个字段的container的选择器
+        field_item_selector : '.field-item',
+        // 配置每个字段错误信息显示的选择器
+        error_field_selector : '.error'
+    },
+    validator_key_selector_map : {
+        // 配置必填字段的选择器
+        required : '.required',
+        // 配置数字的选择器
+        number : '.number',
+        // 配置email的选择器
+        email : '.email',
+        // 配置需要匹配正则的选择器
+        match : '.match'
+    },
+    // 自定义验证器
+    field_validator : {
+        // 配置必填字段的回调验证
+        required : function(event) {
+            console.log(event, $(this));
+        },
+        number : function() {},
+        email : function() {},
+        match : function() {}
+    }
+};
+$('#some-form').formValidator(params);
+```
+
---
+++ validate.form.js
@@ -0,0 +1,171 @@
+(function ($) {
+    $.fn.formValidator = function () {
+        if(arguments.length == 0) {
+            arguments = [{}];
+        }
+
+        methods.init.apply($(this), arguments);
+        $(this).each(function() {
+            $(this).on('submit', function() {
+                var options = $(this).data('options');
+                methods.validate.apply($(this));
+                if(options.validated != 1) {
+                    return false;
+                }
+
+                $(this).submit();
+            });
+        });
+    };
+    var initOptions = {
+        need_validate_selector : '.need-validate',
+        field_item_selector : '.field-item',
+        error_field_selector : '.error'
+    };
+
+    var events = {
+        beforeInit : 'beforeInit',
+        afterInit : 'afterInit',
+        beforeValidate : 'beforeValidate',
+        afterValidate : 'afterValidate',
+        validateField : 'validate.field',
+        beforeValidateField : 'beforeValidateField',
+        afterValidateField : 'afterValidateField'
+    };
+
+    var default_field_validator = {
+        required : function() {
+            $(this).trigger(events.beforeValidateField);
+            var form = $(this).parents('form');
+            var options = form.data('options');
+            if (!options.validated) {
+                return false;
+            }
+            var field_item = $(this).parents(options.field_item_selector);
+            var error_field = field_item.find(options.error_field_selector);
+            if($(this).val() != '') {
+                error_field.addClass('display-none');
+                $(this).trigger(events.afterValidateField);
+                return true;
+            }
+
+            error_field.removeClass('display-none');
+            options.validated = 0;
+            form.data('options', options);
+            $(this).trigger(events.afterValidateField);
+        },
+        number : function() {
+            $(this).trigger(events.beforeValidateField);
+            var form = $(this).parents('form');
+            var options = form.data('options');
+            if (!options.validated) {
+                return false;
+            }
+            var field_item = $(this).parents(options.field_item_selector);
+            var error_field = field_item.find(options.error_field_selector);
+            if($(this).val() % 1 == 0) {
+                error_field.addClass('display-none');
+                $(this).trigger(events.afterValidateField);
+                return true;
+            }
+
+            error_field.html('该字段必须为数字');
+            error_field.removeClass('display-none');
+            options.validated = 0;
+            form.data('options', options);
+            $(this).trigger(events.afterValidateField);
+        },
+        email : function() {
+            $(this).trigger(events.beforeValidateField);
+            var form = $(this).parents('form');
+            var options = form.data('options');
+            if (!options.validated) {
+                return false;
+            }
+            var field_item = $(this).parents(options.field_item_selector);
+            var error_field = field_item.find(options.error_field_selector);
+            var value = $(this).val();
+            var reg = /\w+@\w+\.com/;
+            if (reg.test(value)) {
+                error_field.addClass('display-none');
+                $(this).trigger(events.afterValidateField);
+                return true;
+            }
+
+            error_field.html('该字段必须为邮箱');
+            error_field.removeClass('display-none');
+            options.validated = 0;
+            form.data('options', options);
+            $(this).trigger(events.afterValidateField);
+        },
+        match : function () {
+            $(this).trigger(events.beforeValidateField);
+            var form = $(this).parents('form');
+            var options = form.data('options');
+            if (!options.validated) {
+                return false;
+            }
+
+            var field_item = $(this).parents(options.field_item_selector);
+            var error_field = field_item.find(options.error_field_selector);
+            var value = $(this).val();
+            var pattern = new RegExp($(this).data('pattern'));
+            if (pattern.test(value)) {
+                error_field.addClass('display-none');
+                $(this).trigger(events.afterValidateField);
+                return true;
+            }
+
+            error_field.html('该字段不符合需求');
+            error_field.removeClass('display-none');
+            options.validated = 0;
+            form.data('options', options);
+            $(this).trigger(events.afterValidateField);
+        },
+        beforeValidateField : function() {
+
+        },
+        afterValidateField : function() {
+
+        }
+    };
+
+    var default_filed_validator_key_selector_map = {
+        required : '.required',
+        number : '.number',
+        email : '.email',
+        match : '.match'
+    };
+    
+    var methods = {
+        init : function(params) {
+            return $(this).each(function () {
+                var form = $(this);
+                form.trigger(events.beforeInit);
+                var options = params.hasOwnProperty('form_options')?params['form_options'] : {};
+                options = $.extend(initOptions, options);
+                $(this).data('options', options);
+
+                var validator_key_selector_map = params.hasOwnProperty('validator_key_selector_map')?params['validator_key_selector_map'] : {};
+                validator_key_selector_map = $.extend(default_filed_validator_key_selector_map, validator_key_selector_map);
+
+                var validator = params.hasOwnProperty('validator')?params['validator'] : {};
+                validator = $.extend(default_field_validator, validator);
+                $.each(validator, function(key, callback) {
+                    form.find(validator_key_selector_map[key]).on(events.validateField, callback)
+                        .on(events.beforeValidateField, validator.beforeValidateField)
+                        .on(events.afterValidateField, validator.afterValidateField);
+                });
+                form.trigger(events.afterInit);
+            });
+        },
+        validate : function() {
+            var form = $(this);
+            form.trigger(events.beforeValidate);
+            var options = form.data('options');
+            options.validated = 1;
+            form.find(options.need_validate_selector).trigger(events.validateField);
+            form.trigger(events.afterValidate);
+        }
+    };
+})(window.jQuery);
